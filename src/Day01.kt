645a57d93c01d08e0f3bbe6d0b3c2190925349db
HEIGE-PCloud <52968553+HEIGE-PCloud@users.noreply.github.com>
2022-12-01 15:25:49 +0000

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,25 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+  fun toCalories(input: List<String>): List<Int> =
+    input.flatMapIndexed { index, x ->
+      when {
+        index == 0 || index == input.lastIndex -> listOf(index)
+        x.isEmpty() -> listOf(index - 1, index + 1)
+        else -> emptyList()
+      }
+    }.windowed(size = 2, step = 2) { (from, to) -> input.slice(from..to) }
+      .map { it.fold(0) { acc, s -> acc + Integer.parseInt(s) } }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+  fun part1(input: List<String>): Int = toCalories(input).max()
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+  fun part2(input: List<String>): Int =
+    toCalories(input).sortedDescending().take(3).sum()
+
+  val testInput = readInput("Day01_test")
+  check(part1(testInput) == 24000)
+  check(part2(testInput) == 45000)
+
+  val input = readInput("Day01")
+  println(part1(input))
+  println(part2(input))
 }
