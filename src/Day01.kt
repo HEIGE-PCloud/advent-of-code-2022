10b06c74c87ecdbea34c24cf60a64dfb05bd28e0
HEIGE-PCloud <52968553+HEIGE-PCloud@users.noreply.github.com>
2022-12-01 20:31:17 +0000

    Day01: clean up code

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,16 +1,26 @@
 fun main() {
-
-  fun toCalories(input: List<String>): List<Int> =
-    input.flatMapIndexed { index, x ->
+  /**
+   * Convert the list of input to a list of calories
+   */
+  fun toCalories(input: List<String>): List<Int> = input
+    .flatMapIndexed { index, x ->
       when {
         index == 0 || index == input.lastIndex -> listOf(index)
         x.isEmpty() -> listOf(index - 1, index + 1)
         else -> emptyList()
       }
-    }.windowed(size = 2, step = 2) { (from, to) -> input.slice(from..to) }
-      .map { it.fold(0) { acc, s -> acc + Integer.parseInt(s) } }
+    }
+    .windowed(size = 2, step = 2) { (from, to) ->
+      input.slice(from..to)
+    }
+    .map {
+      it.fold(0) { acc, s ->
+        acc + Integer.parseInt(s)
+      }
+    }
 
-  fun part1(input: List<String>): Int = toCalories(input).max()
+  fun part1(input: List<String>): Int =
+    toCalories(input).max()
 
   fun part2(input: List<String>): Int =
     toCalories(input).sortedDescending().take(3).sum()
